a953379d06ec94f05e723308aa5c70de4ab4fb94
TheGreatOracle <32422537+TheGreatOracle@users.noreply.github.com>
2017-12-02 18:26:49 +0300

    Готовая

--- box/js/main.js
+++ box/js/main.js
@@ -2,10 +2,13 @@
 // 	console.log("Зря кликнул");
 // }
 var presents = ["Спинер", "Ничего, вы были плохим мальчиком", "50 рублей на телефон", "Водка(Русская)", "Мандарины", "Тортик", "Пятерка по информатике", "iPhone из китая", "Пушистые гантели", "Шавуха", "Право владением шаурмечной на молодежке", "Собака из березы", "Мухтар из мухаморов", "Манипулятор энергетического поля нулевого уровня"]
+var imgs = ['spiner.png', 'plohoy.png', '50rub.png','vodka.png', 'mandariny.png', 'tort.png', '5.png', 'iphone.png', 'gant.png', 'shavyha.png', 'shayrmechnaya.png', 'dog.png', 'myhtar.png', 'gun.png']
 $(".start").click(function(){
-	var path = 'png/';
+	var path = 'imgs/';
 	var rand = Math.floor(Math.random() * 14);
-	console.log(presents[rand]);
+	// console.log(presents[rand]);
+	$('.header').text("Поздравляем!");
 	$('.gift').text("Ваш подарок:"+presents[rand]);
+	$('.start').attr('src', path+imgs[rand]);
 
 })
