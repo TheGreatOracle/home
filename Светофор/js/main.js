cb721eacd1ae4fdc8a92c05450f9e3f0856cf50e
TheGreatOracle <32422537+TheGreatOracle@users.noreply.github.com>
2017-12-03 18:24:05 +0300

    Светофор

---
+++ Светофор/js/main.js
@@ -0,0 +1,10 @@
+// var info=$("info").val();
+// 		console.log(info);
+
+// $(".act").click(function(){
+// 	if 
+// 	$(".")
+$(".btn").click(function(){
+	var info = $('.info').val();
+	console.log();
+})
